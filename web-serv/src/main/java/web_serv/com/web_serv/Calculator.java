073b84ec9bcb76c4d098e27e0dacaa36eae2e7b9
hammemi nour allah <hammemi.nourallah@gmail.com>
2024-05-29 15:52:08 +0200

    Initial commit

---
+++ web-serv/src/main/java/web_serv/com/web_serv/Calculator.java
@@ -0,0 +1,10 @@
+package web_serv.com.web_serv;
+
+/**
+ * Simple calculator interface.
+ */
+public interface Calculator {
+    int add(int x, int y);
+    int multiply(int x, int y);
+    void save(int x);
+}
